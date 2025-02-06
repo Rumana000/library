54a469b73e8c31a8d44731429aa5d4ccd8827d6a
Shaik Umayrumaniya <153263326+Rumana000@users.noreply.github.com>
2025-02-06 19:25:58 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,11 +6,11 @@ def maven()
 {
   sh 'mvn package'
 }
-def deploy(jobname,ip,context)
+def newdeploy(jobname,ip,context)
 {
   scp "/var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
-def test(jobname)
+def selenium(jobname)
 {
   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
